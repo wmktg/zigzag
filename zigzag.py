3dfc796b02acfc778dada290ae7bcce12c313942
wooj <wooj.kim3@gmail.com>
2020-03-06 13:41:44 -0500

    fixed some lines of code

--- zigzag.py
+++ zigzag.py
@@ -7,18 +7,18 @@ indentIncreasing = True # whether the indentation is increasing or not
 try: 
     while True: # the main program loop 
         print(' ' * indent, end='')
-        print("********")
+        print('********')
         time.sleep(0.1) # pause for 1/10 of a second 
 
-    if indentIncreasing: 
-        indent = indent + 1 # increase the number of spaces 
-        if indent == 20:
-            indentIncreasing = False # change direction
+        if indentIncreasing: 
+            indent = indent + 1 # increase the number of spaces 
+            if indent == 20:
+                indentIncreasing = False # change direction
     
-    else:
-        indent = indent - 1 # decrease the number of spaces 
-        if indent == 0: 
-            indentIncreasing = True # change direction 
+        else:
+            indent = indent - 1 # decrease the number of spaces 
+            if indent == 0: 
+                indentIncreasing = True # change direction 
 
 except KeyboardInterrupt:
     sys.exit()
